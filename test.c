279f6b548b07c049c80cd4911f2451ceddd9be82
RKR21 <raofm@icloud.com>
2023-01-01 11:30:48 -0500

    Almost done. Just need to do the get_winner function to count the votes

--- test.c
+++ test.c
@@ -1,5 +1,51 @@
-#include "blockchain.h"
 #include <stdbool.h>
+#include "blockchain.h"
+
+void get_winner(chain, candidates[num_candidates][30]){
+    
+}
+
+void election(struct Blockchain * chain, int num_candidates, char candidates[num_candidates][30]){
+    char vote[30] = "";
+    char name[30] = "";
+    do{
+        printf("Enter your name: ");
+        fgets(name, 30, stdin);
+        printf("%s", name);
+        if(strcmp(name, "END\n") == 0){
+            printf("List of votes...\n");
+            printf_blockchain(chain);
+            printf("\n");
+            verify(chain);
+            break;
+        }
+
+        printf("Cast your vote...\n");
+        printf("Candidates:\n");
+        
+        for(int i = 0; i < num_candidates; i++){
+            printf("%s", candidates[i]);
+        }
+        fgets(vote, 30, stdin);
+        // check if valid candidate
+        if(strcmp(vote, "END\n") == 0){
+            printf("List of votes...\n");
+            printf_blockchain(chain);
+            printf("\n");
+            verify(chain);
+            break;
+        }
+        
+        if(strcmp(vote, candidates[0])==0||strcmp(vote, candidates[1])==0||strcmp(vote, candidates[2])==0
+        ||strcmp(vote, candidates[3])==0||strcmp(vote, candidates[4])==0||strcmp(vote, candidates[5]) == 0){
+            add(chain, vote, name);
+            printf("Thank you for voting. Type <END> to end this election.\n");
+        } else{
+            printf("Invalid candidate... Recast vote.\n");
+        }
+        } while(strcmp(vote, "END") != 0 || strcmp(name, "END") != 0);
+    }
+
 
 int main(void){
     char position[30];
@@ -33,5 +79,7 @@ int main(void){
         count++;
     }
     printf("%s\n", candidates[0]);
+    struct Blockchain* chain = initialize();
+    election(chain, num_candidates, candidates);
 }
     
